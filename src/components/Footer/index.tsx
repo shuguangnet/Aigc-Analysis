07776bee0a5cd814a05554656f20e1127ae92514
Shu Guang <61069967+shuguangnet@users.noreply.github.com>
2025-04-17 22:19:15 +0800

    feat: 添加footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,32 +1,43 @@
-import { GithubOutlined } from '@ant-design/icons';
+import { GithubOutlined, HeartFilled, WechatOutlined, MailOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-components';
 import React from 'react';
 
 const Footer: React.FC = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <DefaultFooter
+      
       style={{
-        background: 'none',
+        
+        color: '#fff',
+        padding: '24px 0',
       }}
+      copyright={
+        <div>
+          <div >
+            Copyright © 2023-{currentYear} 陕西科技大学镐京学院
+          </div>
+          <div >
+            Powered by <HeartFilled  /> SunHe
+          </div>
+         
+        </div>
+      }
       links={[
         {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
+          key: 'home',
+          title: '镐京学院',
+          href: 'https://www.sust.edu.cn/',
           blankTarget: true,
         },
         {
           key: 'github',
           title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
+          href: 'https://github.com/shuguangnet',
           blankTarget: true,
         },
-        {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
-          blankTarget: true,
-        },
+       
       ]}
     />
   );
